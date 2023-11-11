649a867bc941e0d27d7f9ba5b0c13d5a75f27f91
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-11-11 13:57:12 +0530

    Add webpack asset configuration for CSS

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,13 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test:  /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     }
 }
