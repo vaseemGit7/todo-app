53b90963d3dab03d5a5dd389ff3bf575ce0052ce
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-11-15 19:34:53 +0530

    Create basic functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Test successful");
+const myTasks = [];
+
+class Task {
+    constructor(title, description, date){
+        this.title = title;
+        this.description = description;
+        this.date = date;  
+    }
+}
+
+function getTasks(){
+    const title = prompt("Enter the task title");
+    const description = prompt("Enter the task description");
+    const date = parseInt(prompt("Enter the task's date"));
+    return new Task(title,description,date);
+}
+
+function addTasks(){
+    return myTasks.push(getTasks());
+}
+
+addTasks();
+addTasks();
+
+console.log(myTasks);
